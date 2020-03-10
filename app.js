4261c1f254f8b11a23cf762991ae9c59d0a58919
fatherflemteacher <34161191+fatherflemteacher@users.noreply.github.com>
2020-03-10 10:14:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -35,10 +35,7 @@ client.on("message",  message => {
   // Let's go with a few common example commands! Feel free to delete or change those.
   
   if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m =  message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+   message.channel.send("pong")
   }
   
   if(command === "say") {
